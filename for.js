b98391f92685762c47f26164d2e2b8f1b7ee16e0
Houda Jawad <houdajawad@gmail.com>
2022-12-07 17:18:09 -0800

    Add answers for statement practice

--- for.js
+++ for.js
@@ -3,12 +3,20 @@
   
 // Challenge 1
 // Use a for statement to log to the console all even numbers between 25 and 75.
-
+for(var i = 25; i < 75; i++){
+  if(i %2 === 0){
+    console.log(i);
+  }
+}
 
 
 // Challenge 2
 // Use a for statement to log to the console a complete sentence stating the weather using the Array below:
 var weather_options = ["sunny", "snowy", "freezing", "partly cloudy", "drizzly"]
+
+for(var i =0; i < weather_options.length; i++){
+    console.log(`its going to be ${weather_options[i]}`);
+}
 
 
 // Challenge 3
@@ -20,3 +28,8 @@ var words = ["car", "bike", "bus", "van", "scooter", "truck", "rv"]
   // Use technical vocabulary and explain each part to demonstrate your understanding! 
   // Share the link to your video in your small group channel.
 
+for(var i = 0; i < words.length; i++){
+  if(words[i].length === 3){
+    console.log(words[i]);
+  }
+}
